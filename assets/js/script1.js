8464803605c3d73af13deb1ba6557c7a16f31fa4
Andrea Iglio <andrea.iglio@gmail.com>
2020-11-22 17:50:35 +0100

    Implemented another js file to use FOR loop for palindrome

---
+++ assets/js/script1.js
@@ -0,0 +1,29 @@
+// // PALINDROMA
+
+// // Chiedere all’utente di inserire una parola
+
+// var userWord = prompt("Inserisci una parola");
+// console.log(userWord);
+
+// UTILIZZO DI FOR LOOP
+
+// var userWordReverse = [];
+
+// for (i = userWord.length; i > (userWord.length - 1); i--){
+
+//     userWordReverse.push(userWord.split('').reverse().join(''));
+    
+
+
+// }
+
+
+// console.log(userWordReverse);
+
+
+
+// if (userWord === userWordReverse[0]){
+//     console.log('The word is palindrome');
+// }
+
+// else (console.log('The word is not palindrome'))
